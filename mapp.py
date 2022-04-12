ef0ad26d5aee55b692ea848c1e9dd37dbd076364
Samyuktha Kapoor <102788315+SamyukthaKapoor@users.noreply.github.com>
2022-04-12 10:41:41 +0530

    Update mapp.py

--- mapp.py
+++ mapp.py
@@ -6,5 +6,5 @@ lon=list(data["LON"])
 lat=list(data["LAT"])
 map=folium.Map(location=[38.91,-99.07],zoom_start=6)
 for lt , ln in zip(lat, lon):
-    map.add_child(folium.Marker(location=[lt,ln], popup="volcano", icon=folium.Icon(color='green')))
+    map.add_child(folium.Marker(location=[lt,ln], popup="volcano", icon=folium.Icon(color='pink')))
 map.save("pandas.html")
